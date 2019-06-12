26324688c057fd38df0491388a1d1d23ef9b2183
Donagh Horgan <donagh.horgan@jci.com>
2019-06-12 01:33:33 +0100

    Allow enabling/disabling showing the scrubbed tweet

--- nodes/pprint.py
+++ nodes/pprint.py
@@ -5,9 +5,10 @@ from .twitter import get_tweet_text
 
 class PrettyPrinter:
 
-    def __init__(self, pos=1 / 3, neg=-1 / 3):
+    def __init__(self, pos=1 / 3, neg=-1 / 3, show_scrubbed=False):
         self.pos = pos
         self.neg = neg
+        self.show_scrubbed = show_scrubbed
 
     def get_colour(self, sentiment):
         if sentiment > self.pos:
@@ -22,7 +23,7 @@ class PrettyPrinter:
 
         text = '-' * 80 + '\n\n'
         text += 'Original tweet: ' + get_tweet_text(data['status']) + '\n\n'
-        text += 'Scrubbed tweet: ' + data['text'] + '\n\n'
+        text += 'Scrubbed tweet: ' + data['text'] + '\n\n' if self.show_scrubbed else ''
         text += 'Sentiment: {}'.format(data['sentiment']['score']) + '\n\n'
         text += '-' * 80
 
